ac566f64a44e5211752dd63ab68874569c953b2b
Nguyễn Tuấn <56992419+XemXongXoa@users.noreply.github.com>
2022-05-18 20:59:09 +0700

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -49,16 +49,12 @@ const nav = document.querySelector("nav");
 
 // =======================================================================//
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    
+  });
 // =================================SLIDE=================================//
